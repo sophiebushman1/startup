9d5d2ccedd270b5f8426089f1636f7553e285881
Bushman <bushman@MacBook-Pro.local>
2024-12-09 15:40:48 -0700

    frontend folder

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import CreateAccount from './createaccount';  // Import CreateAccount component
+import Login from './login';  // Import Login component
+import Browse from './Browse';  // Import the Browse component if needed
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | 
+        <Link to="/signup">Sign Up</Link> | 
+        <Link to="/login">Login</Link> |
+        <Link to="/browse">Browse</Link> {/* Only if Browse is part of the app */}
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<h1>Welcome to the Homepage!</h1>} />
+        <Route path="/browse" element={<Browse />} />  {/* Route for the Browse page */}
+        <Route path="/signup" element={<CreateAccount />} /> {/* Route for CreateAccount */}
+        <Route path="/login" element={<Login />} /> {/* Route for Login */}
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
+
+
